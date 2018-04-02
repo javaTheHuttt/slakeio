a7d877d19789623c274eb98407796fd1d7b0eeee
Cora GlaAA <glac49@guangzhou.imp.fu-berlin.de>
2018-04-02 15:27:12 +0200

    neue Einrückung	geändert:       slake.c

--- slake.c
+++ slake.c
@@ -10,14 +10,14 @@ void move(struct slake)
 	enum slake_mode mode = slake->mode;
 	double delta_distance = speed*lastmove_clock;
 	//new array (new head + old cells-1)
-		struct slake_position *newcells;
-		if(mode == left) {*newcells = head->x-delta_distance;}
-		else if(mode == up) {*newcells = head->y+delta_distance;}
-		else if(mode == right) {*newcells = head->x+delta_distance;}
-		else {*newcells = head->y-delta_distance;}
-		for(int i = 1, i <(myslake->length), i++) {newcells[i] = cells[i-1];}
-		free(cells);
-		cells = newcells;
-		slake->lastmove_clock = clock();
+	struct slake_position *newcells;
+	if(mode == left) {*newcells = head->x-delta_distance;}
+	else if(mode == up) {*newcells = head->y+delta_distance;}
+	else if(mode == right) {*newcells = head->x+delta_distance;}
+	else {*newcells = head->y-delta_distance;}
+	for(int i = 1, i <(myslake->length), i++) {newcells[i] = cells[i-1];}
+	free(cells);
+	cells = newcells;
+	slake->lastmove_clock = clock();
 }
 
