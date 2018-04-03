bdbda9a02a0b57b732ba16140fa7537e266448e9
Tim Rademacher <timr77@guangzhou.imp.fu-berlin.de>
2018-04-03 12:55:34 +0200

    Updated move logic and array handling

--- slake.c
+++ slake.c
@@ -1,23 +1,45 @@
-#include <clock.h>
-int main(int argc, char *argv []) {
-return 0;
-}
+#include <stdlib.h>
+#include <time.h>
+#include "slakeio.h"
 
-//move
-void move(struct slake)
+void slake_move(struct slake_t *slake)
 {
-	struct slake_position head = slake->cells;
-	enum slake_mode mode = slake->mode;
-	double delta_distance = speed*lastmove_clock;
-	//new array (new head + old cells-1)
-	struct slake_position *newcells;
-	if(mode == left) {*newcells = head->x-delta_distance;}
-	else if(mode == up) {*newcells = head->y+delta_distance;}
-	else if(mode == right) {*newcells = head->x+delta_distance;}
-	else {*newcells = head->y-delta_distance;}
-	for(int i = 1, i <(myslake->length), i++) {newcells[i] = cells[i-1];}
-	free(cells);
-	cells = newcells;
+	// points to first position of slake head
+	struct slake_position_t *head = slake->cells;
+	
+	// current slake mode
+	enum slake_mode_t mode = slake->mode;
+	
+	// expected distance since last move
+	double delta_distance = slake->speed * slake->lastmove_clock;
+	
+	// storage management
+	head = realloc(head, sizeof(struct slake_position_t) * slake->length);
+		
+	// new head positions
+	if(mode == left) {
+		head->x = (head->x) - delta_distance;
+		head->y = (head->y);
+	}
+	else if(mode == up) {
+		head->x = head->x;
+		head->y = (head->y) + delta_distance;
+	}
+	else if(mode == right) {
+		head->x = (head->x) + delta_distance;
+		head->y = head->y;
+	}
+	else {	// down
+		head->x = head->x;
+		head->y = (head->y) - delta_distance;
+	}
+	
+	// move slake tail  
+	for(int i = 1; i < slake->length; ++i) {
+		slake->cells[i] = slake->cells[i-1];
+	}
+	
+	// update clock after move
 	slake->lastmove_clock = clock();
 }
 
