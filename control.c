2d94b4cbb5558b232c9d4d7a1ac1c25e5499c097
Cora GlaAA <glac49@shenyang.imp.fu-berlin.de>
2018-04-03 12:10:41 +0200

    defi key_control function	neue Datei:     control.c

---
+++ control.c
@@ -0,0 +1,15 @@
+// 
+// Control (Keys etc.)
+//
+
+#include "slakeio.h"
+#include "termbox.h"
+
+static enum slake_mode key_control(e.type)
+{
+	if(e.type == TB_KEY_LEFT) {  *my_slake->slake_mode =left; }	
+	if(e.type == TB_KEY_UP) { *my_slake->slake_mode =up; }
+	if(e.type == TB_KEY_RIGHT) { *my_slake->slake_mode =right; }
+	if(e.type == TB_KEY_DOWN) { *my_slake->slake_mode =down; }
+	}
+}
