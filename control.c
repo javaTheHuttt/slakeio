548c0f419d2eae6b289814aa84b2891c89fe220b
Cora GlaAA <glac49@shenyang.imp.fu-berlin.de>
2018-04-03 14:27:47 +0200

    directions contol check	geändert:       control.c

--- control.c
+++ control.c
@@ -9,10 +9,10 @@ void key_control(uint16_t key)
 {
  	enum slake_mode_t old_mode = my_slake->mode; //mode right before a change
 	// 	checks that modechange is possible
-	if(old_mode == left && key == TB_KEY_ARROW_RIGHT){;}
- 	if(old_mode == right && key == TB_KEY_ARROW_LEFT){;}
-	if(old_mode == up && key == TB_KEY_ARROW_DOWN){;}
-	if(old_mode == down && key == TB_KEY_ARROW_UP){;}
+	if((old_mode == left && key == TB_KEY_ARROW_RIGHT) || (old_mode == left && key == TB_KEY_ARROW_LEFT)){;}
+ 	if((old_mode == right && key == TB_KEY_ARROW_LEFT) || (old_mode == right && key == TB_KEY_ARROW_RIGHT)){;}
+	if((old_mode == up && key == TB_KEY_ARROW_DOWN) || (old_mode == up && key == TB_KEY_ARROW_UP)){;}
+	if((old_mode == down && key == TB_KEY_ARROW_UP) || (old_mode == down && key == TB_KEY_ARROW_DOWN)){;}
 	//	changes mode
 	if(key == TB_KEY_ARROW_LEFT) { my_slake->mode =left; }	
 	if(key == TB_KEY_ARROW_UP) { my_slake->mode =up; }
