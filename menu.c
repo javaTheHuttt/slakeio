48130026b4a2f4e34820e9348aa922ba467080b2
Cora GlaAA <glac49@andorra.imp.fu-berlin.de>
2018-04-04 23:38:00 +0200

    geändert:       menu.c

--- menu.c
+++ menu.c
@@ -3,28 +3,47 @@
 #include <string.h>
 #include <unistd.h>
 
+enum gamestatus {
+	startmenu,
+	playing,
+	pausemenu,
+	win,
+	lost};
+
+
 struct tb_cell menu = {
 	.ch = ' ',
 	.fg = TB_DEFAULT,
 	.bg = TB_BLACK
 };
 
+void put_menu_header(char *option, uint16_t style, int x, int y)
+{
+	int option_str_length = strlen(option);	
+	for(int i = 0; i<option_str_length; i++)
+	{
+		tb_change_cell(x+i,y+2, '+', TB_BLUE, TB_BLACK);
+		tb_change_cell(x+i,y,option[i] , style , TB_BLACK);
+		tb_change_cell(x+i,y-2, '+', TB_BLUE, TB_BLACK);
+	}
+	//seiten
+	for(int j = 0; j<5; j++)
+	{
+		tb_change_cell(x-1,y+2-j, '+', TB_BLUE, TB_BLACK);
+		tb_change_cell(x+option_str_length, y+2-j, '+', TB_BLUE, TB_BLACK);
+	}
+}
+
 void put_menu_option(char *option, uint16_t style, int x, int y)
 {
 	int option_str_length = strlen(option);	
 	for(int i = 0; i<option_str_length; i++)
 	{
-		tb_change_cell(x+i,y+1, '+', TB_BLUE, TB_WHITE);
-		tb_change_cell(x+i,y,option[i] , style , TB_BLUE);
-		tb_change_cell(x+i,y-1, '+', TB_BLUE, TB_WHITE);
-	}
-	//seiten
-	for(int j = 0; j<3; j++)
-	{
-		tb_change_cell(x-1,y+1-j, '+', TB_BLUE, TB_WHITE);
-		tb_change_cell(x+option_str_length, y+1-j, '+', TB_BLUE, TB_WHITE);
+		tb_change_cell(x+i,y,option[i] , style , TB_BLACK);
+		tb_change_cell(x+i,y+1, '+', TB_BLUE, TB_BLACK);
 	}
 }
+
 
 void put_background(int start_x, int start_y,int end_x,int end_y)
 {
@@ -42,21 +61,23 @@ int main(void)
 {
 	tb_init();
 
+
 	while(1) {
 
 		//stuff to calculate the center & enter the strings
 		int width = tb_width();
 		int height = tb_height();	
-		put_background(0,0,width,height);
-		char *welcome = "Willkommen in SlakeWorld!";
-		char *start = "Start";
+		char *welcome = "  Willkommen in SlakeWorld!  ";
+		char *start = " Start ";
 		int welcome_lenght = strlen(welcome);
 		int start_lenght = strlen(start);
 		int welcome_height = height/2;
 		int welcome_begin = width/2 -(welcome_lenght/2);
 		int start_begin = (welcome_lenght/2) - (start_lenght/2);
-		put_menu_option(welcome, TB_BLACK ,welcome_begin, welcome_height);
-		put_menu_option(start, TB_BLACK, welcome_begin+start_begin,welcome_height+5
+		put_background(0,0,width,height);
+
+		put_menu_header(welcome, TB_BLUE ,welcome_begin, welcome_height);
+		put_menu_option(start, TB_BLUE, welcome_begin+start_begin,welcome_height+5
 );
 
 		tb_present();
@@ -65,7 +86,7 @@ int main(void)
 		struct tb_event input;
 		char *letsgo = "LETS GO";
 		
-		struct tb_event begin;
+
 
 		tb_peek_event(&input, 100);
 		switch(input.key){
@@ -74,21 +95,33 @@ int main(void)
 			case TB_KEY_ENTER:
 				put_menu_option(start, TB_GREEN, welcome_begin+start_begin,welcome_height+5);
 				tb_present();
-				tb_peek_event(&begin, 100);
-				switch(begin.key){
-				case TB_KEY_ESC:
-					goto exit;
-				case TB_KEY_ENTER:
-					goto game;
-				}
+				break;
+			//	goto game;
+			
 			}
 		
 	
 	}		
-game:
-					put_menu_option("Ha", TB_RED, 1,1);
-			pause();
+/*game:
+			tb_peek_event(&begin, 100);
+		int width = tb_width();
+		int height = tb_height();	
+		char *welcome = "Willkommen in SlakeWorld!";
+		char *start = "Start";
+		int welcome_lenght = strlen(welcome);
+		int start_lenght = strlen(start);
+		int welcome_height = height/2;
+		int welcome_begin = width/2 -(welcome_lenght/2);
+		int start_begin = (welcome_lenght/2) - (start_lenght/2);
+	switch(begin.key){
+				case TB_KEY_ESC:
+					goto exit;
+				case TB_KEY_ENTER:
+					put_menu_option(start, TB_RED, welcome_begin+start_begin,welcome_height+5);
+					tb_present();
 
+				}
+*/
 exit:
 	tb_shutdown();
 return(0);
