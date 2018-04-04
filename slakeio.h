2eccd40df7a5ab8dba493b952f4ccf9291acea66
Tim Rademacher <tim.rademacher@kabelmail.de>
2018-04-04 11:52:26 +0200

    Removed log function herader

--- slakeio.h
+++ slakeio.h
@@ -91,14 +91,7 @@ void draw(void);
 //
 //checks and changes mode (direction of slake)
 void key_control(uint16_t key);
-// log function to write something to the log.
-void slakeio_log(char *str);
 // a fuction that checks weather a position is inside a givin rect 
 // returns true if it's inside
 int in_rect(struct slake_map_t *rect, struct slake_position_t *pos);
-
-
-
-
-
 
