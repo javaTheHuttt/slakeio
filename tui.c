bedfa3b1364861ee4df9093603cb43eaa57228ab
Jakob Heinrich <jakob.heinrich@fu-berlin.de>
2018-04-05 11:50:01 +0200

    added constant variables for map size & amount food

--- tui.c
+++ tui.c
@@ -97,33 +97,10 @@ void show_coordinates(int x, int y) {
 
 
 void draw(void) {
-	// ################## DRAW SLAKES #########################
+	tb_clear();
 	struct slake_map_t *screen = get_screen();
-	// go with s through all slakes
-
-	
 	int x;
 	int y;
-	
-	//write_log("Screen Position\n upper_left:\n  x: %d\n  y: %d\n\n bottom_right: \n  x: %d\n  y: %d\n", screen->upper_left->x, screen->upper_left->y, screen->bottom_right->x, screen->bottom_right->y);
-
-	tb_clear();
-	for(int s = 0; s < all_slakes->length; s++) {
-		// go with c through all cells of the slake
-		for (int c = 0; c < (all_slakes->array[s]).length; c++) {
-			if (in_rect(screen, &(all_slakes->array[s]).cells[c])) {
-				x = ((all_slakes->array[s]).cells[c]).x - screen->upper_left->x;
-				y = ((all_slakes->array[s]).cells[c]).y - screen->upper_left->y;
-				tb_put_cell(x, y, &myslake_player);
-				
-			}
-	
-		}
-	}
-	
-
-
-
 	// ####################### DRAW FOOD ########################
 	
 	
@@ -159,6 +136,32 @@ void draw(void) {
 	}
 	// ################ SHOW COORDINATES ######################
 	show_coordinates(my_slake->cells[0].x, my_slake->cells[0].y);
+
+
+	// ################## DRAW SLAKES #########################
+
+	// go with s through all slakes
+
+	
+
+	
+	//write_log("Screen Position\n upper_left:\n  x: %d\n  y: %d\n\n bottom_right: \n  x: %d\n  y: %d\n", screen->upper_left->x, screen->upper_left->y, screen->bottom_right->x, screen->bottom_right->y);
+
+	
+	for(int s = 0; s < all_slakes->length; s++) {
+		// go with c through all cells of the slake
+		for (int c = 0; c < (all_slakes->array[s]).length; c++) {
+			if (in_rect(screen, &(all_slakes->array[s]).cells[c])) {
+				x = ((all_slakes->array[s]).cells[c]).x - screen->upper_left->x;
+				y = ((all_slakes->array[s]).cells[c]).y - screen->upper_left->y;
+				tb_put_cell(x, y, &myslake_player);
+				
+			}
+	
+		}
+	}
+	
+
 	
 	tb_present();	
 
